bdfc2a8c7ba3032de86ac8acaeaf75e96acb1e19
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 12:55:08 +0530

    Add files via upload

---
+++ Test4.java
@@ -0,0 +1,15 @@
+//Write a Java program to find the value of specified expression.
+
+
+public class Test4{
+	public static void main(String[] args) {
+	
+System.out.println("101+0/3  =  "+(101+0/3));
+System.out.println(" 3.0e-6 * 10000000.1  = "+(3.0e-6 * 10000000.1));
+System.out.println(" 10>5 && 10!=5  = "+(10>5 && 10!=5));
+System.out.println(" 10<5 && 10>5   = "+(10<5 && 10>5));
+System.out.println(" (10<5 && 10!=5) || (10>5 && 10!=5)   = "+((10<5 && 10!=5) || (10>5 && 10!=5)));
+System.out.println(" (10<5 || 10!=5) && (10>5 && 10!=5)   = "+((10<5 || 10!=5) && (10>5 && 10!=5)));
+	}
+}
+
