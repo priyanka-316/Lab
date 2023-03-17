17cdbb5cb935c610ceaa68258f70819a608f9ac4
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 12:21:38 +0530

    Add files via upload

---
+++ Test1.java
@@ -0,0 +1,14 @@
+//Q.1 Write a Java program to print 'Hello' on screen and then print your details on a separate line.
+class Test1
+{
+
+public static void main(String args[])
+{
+System.out.println("Hello");
+System.out.println("This is Priyanka Jadhav");
+System.out.println("I'm From Solapur");
+System.out.println("Thank You.......!!");
+
+
+}
+}
