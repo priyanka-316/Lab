3ebb74aa260d8692b71de93186ec592d840505b9
priyanka-316 <127471221+priyanka-316@users.noreply.github.com>
2023-03-17 12:45:15 +0530

    Add files via upload

---
+++ Test2.java
@@ -0,0 +1,25 @@
+ //Write a Java program to print the sum (addition), multiply, subtract, divide and remainder of two numbers.
+
+class Test2
+{
+
+public static void main(String args[])
+{
+  
+ 
+  int num1 = 10;
+  
+  int num2 = 20;
+   
+ 
+  System.out.println(num1 + " + " + num2 + " = " +  (num1 + num2));
+   
+  System.out.println(num1 + " - " + num2 + " = " +  (num1 - num2));
+   
+  System.out.println(num1 + " x " + num2 + " = " +  (num1 * num2));
+   
+  System.out.println(num1 + " / " + num2 + " = " +  (num1 / num2));
+ 
+  System.out.println(num1 + " mod " + num2 + " = " + (num1 % num2));
+ }
+}
